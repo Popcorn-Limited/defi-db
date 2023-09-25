424f0b1133e6671a26aa00b78f944c45b4376d37
RedVeil <leon.niesler@gmail.com>
2023-09-25 23:28:10 +0200

    updated sdk to 0.0.27 and added correct data

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
 import { createAnvil } from "@viem/anvil";
-import { YieldOptions, ProtocolProvider } from "vaultcraft-sdk";
+import { YieldOptions, LiveProvider } from "vaultcraft-sdk";
 import { createPublicClient, http } from "viem";
 import { existsSync, mkdirSync, renameSync, writeFileSync } from "fs";
 import dayjs from "dayjs";
 
 const ARCHIVE_PATH = "./archive";
 (async () => {
-    console.log("moving current file to archive");
-    const date = dayjs().subtract(1, 'day').format("YYYY-MM-DD");
+  console.log("moving current file to archive");
+  const date = dayjs().subtract(1, "day").format("YYYY-MM-DD");
 
-    if (!existsSync(ARCHIVE_PATH)) {
-        mkdirSync(ARCHIVE_PATH);
+  if (!existsSync(ARCHIVE_PATH)) {
+    mkdirSync(ARCHIVE_PATH);
+  }
+  renameSync("./apy-data.json", `${ARCHIVE_PATH}/${date}.json`);
+
+  // we use a local anvil instance to decrease the number of RPC requests sent to a public endpoint.
+  const anvil = createAnvil({
+    forkUrl: "https://eth.llamarpc.com",
+  });
+  const publicClient = createPublicClient({
+    chain: anvil,
+    mode: "anvil",
+    transport: http("http://127.0.0.1:8545"),
+  });
+  console.log("starting anvil instance");
+  await anvil.start();
+
+  const provider = new LiveProvider({ 1: publicClient }, 10000);
+  const yieldOptions = new YieldOptions(provider, 1000);
+  const result = {
+    1: {},
+  };
+  for (const { name, key, logoURI, description, tags } of provider.getProtocols(
+    1
+  )) {
+    console.log(`pulling yield data for ${key}`);
+    result[1][key] = {};
+    try {
+      const protocolData = await yieldOptions.getYieldOptionsByProtocol(
+        1,
+        key
+      );
+      protocolData.forEach((data) => {
+        result[1][key][data.asset] = data.yield;
+      });
+    } catch (e) {
+      console.log("failed to pull yield data for ", key);
+      console.error(e);
     }
-    renameSync("./apy-data.json", `${ARCHIVE_PATH}/${date}.json`);
+  }
+  console.log("stopping anvil instance");
+  await anvil.stop();
 
-    // we use a local anvil instance to decrease the number of RPC requests sent to a public endpoint.
-    const anvil = createAnvil({
-        forkUrl: "https://eth.llamarpc.com",
-    });
-    const publicClient = createPublicClient({
-        chain: anvil,
-        mode: "anvil",
-        transport: http("http://127.0.0.1:8545"),
-    });
-    console.log("starting anvil instance");
-    await anvil.start();
-
-    const provider = new ProtocolProvider({ 1: publicClient }, 10000);
-    const yieldOptions = new YieldOptions(provider, 1000);
-    const result = {
-        1: {},
-    }
-    for (const protocol of provider.getProtocols(1)) {
-        console.log(`pulling yield data for ${protocol}`);
-        result[1][protocol] = {};
-        try {
-
-            const protocolData = await yieldOptions.getYieldOptionsByProtocol(1, protocol);
-            protocolData.forEach((data) => {
-                result[1][protocol][data.address] = data.yield;
-            });
-        } catch (e) {
-            console.log("failed to pull yield data for ", protocol);
-            console.error(e);
-        }
-    }
-    console.log("stopping anvil instance");
-    await anvil.stop();
-
-    console.log("saving result in apy-data.json");
-    writeFileSync("./apy-data.json", JSON.stringify(result), "utf-8");
+  console.log("saving result in apy-data.json");
+  writeFileSync("./apy-data.json", JSON.stringify(result), "utf-8");
 })();
