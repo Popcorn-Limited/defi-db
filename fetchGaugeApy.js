2f360dbfad665f499caeffec188262dcca7b9d6b
RedVeil <leon.niesler@gmail.com>
2024-05-16 11:20:24 +0200

    daily apy

--- fetchGaugeApy.js
+++ fetchGaugeApy.js
@@ -142,13 +142,13 @@ async function getTokenPrice(token, chainId) {
 async function getVCXPrice() {
   try {
     const { data } = await axios.get(
-      "https://api.dexscreener.com/latest/dex/pairs/ethereum/0x577a7f7ee659aa14dc16fd384b3f8078e23f1920000200000000000000000633-0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2-0xcE246eEa10988C495B4A90a905Ee9237a0f91543"
+      "https://api.dexscreener.com/latest/dex/pairs/ethereum/0x577A7f7EE659Aa14Dc16FD384B3F8078E23F1920"
     );
 
     return Number(data.pair.priceUsd);
   } catch (e) {
     console.log("error fetching vcx price: ", e);
-    return 0.08473;
+    return 0.09412;
   }
 }
 
